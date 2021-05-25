bbc0222cf7125281561d810017a700796306a47a
Jirka Humpl <jriihumpl@gmail.com>
2021-05-25 20:21:46 +0200

    try pisa's main for lcd

--- main.c
+++ main.c
@@ -1,18 +1,3 @@
-/*******************************************************************
-  Project main function template for MicroZed based MZ_APO board
-  designed by Petr Porazil at PiKRON
-
-  change_me.c      - main file
-
-  include your name there and license for distribution.
-
-  Remove next text: This line should not appear in submitted
-  work and project name should be change to match real application.
-  If this text is there I want 10 points subtracted from final
-  evaluation.
-
- *******************************************************************/
-
 #define _POSIX_C_SOURCE 200112L
 
 #include <stdlib.h>
@@ -24,37 +9,84 @@
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
 #include "mzapo_regs.h"
-#include "serialize_lock.h"
 
 int main(int argc, char *argv[])
 {
-  parlcd_hx8357_init("co je to mem_base a jak to ziskam");
+    unsigned char *mem_base;
+    unsigned char *parlcd_mem_base;
+    uint32_t val_line = 5;
+    int i, j, k;
+    unsigned int c;
 
-  /* Serialize execution of applications */
+    printf("Hello world\n");
 
-  /* Try to acquire lock the first */
-  if (serialize_lock(1) <= 0) {
+    sleep(1);
 
-    void parlcd_write_data("co je to mem_base a jak to ziskam", "System is occupied\n");
-    printf("System is occupied\n");
+    /*
+   * Setup memory mapping which provides access to the peripheral
+   * registers region of RGB LEDs, knobs and line of yellow LEDs.
+   */
+    mem_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
 
-    if (1) {
-      printf("Waitting\n");
-      /* Wait till application holding lock releases it or exits */
-      serialize_lock(0);
+    /* If mapping fails exit with error code */
+    if (mem_base == NULL)
+        exit(1);
+
+    struct timespec loop_delay = {.tv_sec = 0, .tv_nsec = 20 * 1000 * 1000};
+    for (i = 0; i < 30; i++)
+    {
+        *(volatile uint32_t *)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
+        val_line <<= 1;
+        printf("LED val 0x%x\n", val_line);
+        clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
     }
-  }
 
-  printf("Hello world\n");
-  void parlcd_write_data("co je to mem_base a jak to ziskam", "Hello world\n");
+    parlcd_mem_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
 
-  sleep(4);
+    if (parlcd_mem_base == NULL)
+        exit(1);
 
-  printf("Goodbye world\n");
-  void parlcd_write_data("co je to mem_base a jak to ziskam", "Goodbye world\n");
+    parlcd_hx8357_init(parlcd_mem_base);
 
-  /* Release the lock */
-  serialize_unlock();
+    parlcd_write_cmd(parlcd_mem_base, 0x2c);
+    for (i = 0; i < 320; i++)
+    {
+        for (j = 0; j < 480; j++)
+        {
+            c = 0;
+            parlcd_write_data(parlcd_mem_base, c);
+        }
+    }
 
-  return 0;
+    parlcd_write_cmd(parlcd_mem_base, 0x2c);
+    for (i = 0; i < 320; i++)
+    {
+        for (j = 0; j < 480; j++)
+        {
+            c = ((i & 0x1f) << 11) | (j & 0x1f);
+            parlcd_write_data(parlcd_mem_base, c);
+        }
+    }
+
+    loop_delay.tv_sec = 0;
+    loop_delay.tv_nsec = 200 * 1000 * 1000;
+    for (k = 0; k < 60; k++)
+    {
+
+        parlcd_write_cmd(parlcd_mem_base, 0x2c);
+        for (i = 0; i < 320; i++)
+        {
+            for (j = 0; j < 480; j++)
+            {
+                c = (((i + k) & 0x1f) << 11) | ((j + k) & 0x1f);
+                parlcd_write_data(parlcd_mem_base, c);
+            }
+        }
+
+        clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
+    }
+
+    printf("Goodbye world\n");
+
+    return 0;
 }
