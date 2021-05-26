7cfe9e9a5eb20ae6ef5a4388d97b20573c4b19af
Jirka Humpl <jriihumpl@gmail.com>
2021-05-26 21:45:17 +0200

    added clean function to turn lcd black

--- parlcd.c
+++ parlcd.c
@@ -1,8 +1,32 @@
 #include "parlcd.h"
 #include <stdio.h>
 
-bool parlcd_write_layer()
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 320
+
+void parlcd_write_layer(unsigned char *parlcd_mem_base)
 {
     //probably getting pointer to fst point of movement in layer as a parameter
-    return true;
+    unsigned int lcd[SCREEN_WIDTH][SCREEN_HEIGHT]={0};
+    parlcd_write_cmd(parlcd_mem_base, 0x2c);
+    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    {
+        for (int j = 0; j < SCREEN_WIDTH; j++)
+        {
+            parlcd_write_data(parlcd_mem_base, lcd[SCREEN_WIDTH][SCREEN_HEIGHT]);
+        }
+    }
+}
+
+void parlcd_clean(unsigned char *parlcd_mem_base)
+{
+    parlcd_write_cmd(parlcd_mem_base, 0x2c);
+    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    {
+        for (int j = 0; j < SCREEN_WIDTH; j++)
+        {
+            unsigned int c = 0;
+            parlcd_write_data(parlcd_mem_base, c);
+        }
+    }
 }
--- parlcd.h
+++ parlcd.h
@@ -1,6 +1,7 @@
 #ifndef PARLCD_H
 #define PARLCD_H
 
-bool parlcd_write_layer();
+void parlcd_write_layer(unsigned char *parlcd_mem_base);
+void parlcd_clean(unsigned char *parlcd_mem_base);
 
 #endif
