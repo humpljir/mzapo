08003df58f88d9830c36ea2ed985dd971261459d
Jirka Humpl <jriihumpl@gmail.com>
2021-05-29 15:58:28 +0200

    updated functions in parlcd.h to match parlcd.c

--- parlcd.c
+++ parlcd.c
@@ -2,6 +2,7 @@
 #include "mzapo_parlcd.h"
 #include "font_types.h"
 #include "gcode.h"
+#include "config.h"
 #include <math.h>
 #include <stdio.h>
 
@@ -12,34 +13,25 @@
 #define FONT_SPACING 0
 #define BACKGROUND_COLOR 000000
 
-void parlcd_write_layer(unsigned char *parlcd_mem_base, layer_t layer, unsigned short color)
+void parlcd_clean(unsigned char *parlcd_mem_base)
 {
-    /*
-    //probably getting pointer to fst point of movement in layer as a parameter
-    bool lcd[SCREEN_WIDTH][SCREEN_HEIGHT] = {0};
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
     for (int i = 0; i < SCREEN_HEIGHT; i++)
     {
         for (int j = 0; j < SCREEN_WIDTH; j++)
         {
-            if (lcd[SCREEN_WIDTH][SCREEN_HEIGHT])
-            {
-                unsigned short c = MAIN_COLOR;
-                parlcd_write_data(parlcd_mem_base, c);
-            }
+            unsigned int c = global.color_background;
+            parlcd_write_data(parlcd_mem_base, c);
         }
-    }
-    */
-
-    int i=0;
-
-    for (i=0;i<layer.length;i++)
-    {
-        draw_line(layer.points[i].x,layer.points[i].y,layer.points[i+1].x,layer.points[i+1].y,color);
     }
 }
 
-void draw_char(int x, int y, char ch, unsigned short color, font_descriptor_t *fdes)
+void draw_pixel(int x, int y, unsigned int color)
+{
+    return;
+}
+
+void draw_char(int x, int y, char ch, unsigned int color, font_descriptor_t *fdes)
 {
     int w = fdes->maxwidth;
     const font_bits_t *ptr;
@@ -70,7 +62,7 @@ void draw_char(int x, int y, char ch, unsigned short color, font_descriptor_t *f
     }
 }
 
-void draw_string(int x, int y, char string[], unsigned short color, font_descriptor_t *fdes)
+void draw_string(int x, int y, char string[], unsigned int color, font_descriptor_t *fdes)
 {
     int i = 0;
     while (string[i] != '\0')
@@ -93,9 +85,9 @@ int calculate_constant(int x1, int y1, int x2, int y2)
     return c;
 }
 
-    void draw_line(int x1, int y1, int x2, int y2, unsigned short color)
+void draw_line(int x1, int y1, int x2, int y2, unsigned int color)
 {
-    int c = 0, a = 0, b= 0;
+    int c = 0, a = 0, b = 0;
     calculate_vector(&a, &b, x1, y1, x2, y2);
     c = calculate_constant(a, b, x2, y2);
     if (x1 == x2)
@@ -143,15 +135,29 @@ int calculate_constant(int x1, int y1, int x2, int y2)
     }
 }
 
-void parlcd_clean(unsigned char *parlcd_mem_base)
+void parlcd_write_layer(unsigned char *parlcd_mem_base, layer_t layer, unsigned int color)
 {
+    /*
+    //probably getting pointer to fst point of movement in layer as a parameter
+    bool lcd[SCREEN_WIDTH][SCREEN_HEIGHT] = {0};
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
     for (int i = 0; i < SCREEN_HEIGHT; i++)
     {
         for (int j = 0; j < SCREEN_WIDTH; j++)
         {
-            unsigned int c = BACKGROUND_COLOR;
-            parlcd_write_data(parlcd_mem_base, c);
+            if (lcd[SCREEN_WIDTH][SCREEN_HEIGHT])
+            {
+                unsigned short c = MAIN_COLOR;
+                parlcd_write_data(parlcd_mem_base, c);
+            }
         }
+    }
+    */
+
+    int i = 0;
+
+    for (i = 0; i < layer.length; i++)
+    {
+        draw_line(layer.points[i].x, layer.points[i].y, layer.points[i + 1].x, layer.points[i + 1].y, color);
     }
 }
--- parlcd.h
+++ parlcd.h
@@ -3,10 +3,13 @@
 #ifndef PARLCD_H
 #define PARLCD_H
 
-void parlcd_write_layer(unsigned char *parlcd_mem_base);
 void parlcd_clean(unsigned char *parlcd_mem_base);
-void draw_char(int x, int y, char ch, unsigned short color, font_descriptor_t *fdes);
-void draw_string(int x, int y, char string[], unsigned short color, font_descriptor_t *fdes);
-void draw_line(int x1, int y1, int x2, int y2, unsigned short color);
+void draw_pixel(int x, int y, unsigned int color);
+void draw_char(int x, int y, char ch, unsigned int color, font_descriptor_t *fdes);
+void draw_string(int x, int y, char string[], unsigned int color, font_descriptor_t *fdes);
+void calculate_vector(int *x, int *y, int x1, int y1, int x2, int y2);
+int calculate_constant(int x1, int y1, int x2, int y2);
+void draw_line(int x1, int y1, int x2, int y2, unsigned int color);
+void parlcd_write_layer(unsigned char *parlcd_mem_base, layer_t layer, unsigned int color);
 
 #endif
